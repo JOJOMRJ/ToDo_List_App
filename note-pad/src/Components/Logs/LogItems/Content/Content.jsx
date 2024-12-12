abc1b08bb0761c248e014d9b1aaefd5442106a3f
JoJo <jiaoyalin92@gmail.com>
2024-12-12 16:03:42 +1100

    content完成

--- note-pad/src/Components/Logs/LogItems/Content/Content.jsx
+++ note-pad/src/Components/Logs/LogItems/Content/Content.jsx
@@ -27,7 +27,6 @@ const Content = ({ id,desc, time, onUploadLogs, logsData}) => {
   
   // 通用保存方法
   const saveHandler = (field) => {
-    // 调用对应字段的 cancelHandler 来退出编辑模式
     console.log( '我执行了',field);
     
     const fieldHandler = fieldHandlers[field];
@@ -37,7 +36,7 @@ const Content = ({ id,desc, time, onUploadLogs, logsData}) => {
     console.log( '我执行了+1');
     const updatedLogs = logsData.map((log) => {
       if (log.id === id) {
-        return { ...log, [field]: fieldHandler.getValue() }; // 获取当前值并更新
+        return { ...log, [field]: fieldHandler.getValue() }; 
       }
       return log;
     });
@@ -46,14 +45,14 @@ const Content = ({ id,desc, time, onUploadLogs, logsData}) => {
 
   // 按键处理
   const keyActions = {
-    Enter: (field) => saveHandler(field), // 按下 Enter 保存
-    Escape: (field) => fieldHandlers[field].cancelHandler(), // 按下 Escape 取消
+    Enter: (field) => saveHandler(field), 
+    Escape: (field) => fieldHandlers[field].cancelHandler(), 
   };
 
   const handleKeys = (e, field) => {
     const action = keyActions[e.key];
     if (action) {
-      action(field); // 调用对应的按键操作，并传递 field
+      action(field); 
     }
   };
 
@@ -65,15 +64,15 @@ const Content = ({ id,desc, time, onUploadLogs, logsData}) => {
           className='desc' 
           type="text"
           value={fieldHandlers.desc.getValue()}
-          onChange={(e) => fieldHandlers.desc.setEditing(e.target.value)} // 更新描述
-          onBlur={ () => saveHandler('desc')} // 失去焦点保存
+          onChange={(e) => fieldHandlers.desc.setEditing(e.target.value)} 
+          onBlur={ () => saveHandler('desc')} 
           onKeyDown={(e) => handleKeys(e, 'desc')}
           autoFocus
         />
       ) : (
         <p 
         className='desc'  
-        onClick={() => setIsEditingDesc(true)}>{desc}</p> // 点击进入编辑模式
+        onClick={() => setIsEditingDesc(true)}>{desc}</p> 
       )}
 
       {/* 时长字段 */}
@@ -81,15 +80,15 @@ const Content = ({ id,desc, time, onUploadLogs, logsData}) => {
         <input
           type="number"
           value={editedTime}
-          onChange={(e) => setEditedTime(e.target.value)} // 更新时长
-          onBlur={ () => saveHandler('time')} // 失去焦点保存
-          onKeyDown={(e) => handleKeys(e, 'time')} // 按 Esc 取消
+          onChange={(e) => setEditedTime(e.target.value)} 
+          onBlur={ () => saveHandler('time')} 
+          onKeyDown={(e) => handleKeys(e, 'time')}
           autoFocus
         />
       ) : (
         <p 
           className='time'
-          onClick={() => setIsEditingTime(true)}>{time} m</p> // 点击进入编辑模式
+          onClick={() => setIsEditingTime(true)}>{time} m</p> 
       )}
     </div>
   );
